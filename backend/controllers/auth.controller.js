30cb621c062e270c837406b7be743cfcc28ca3b2
Micheal Ngene <mngene4@gmail.com>
2025-09-29 00:28:28 +0200

    Modified auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -62,6 +62,10 @@ export const login = async (req, res) => {
         //     return res.status(400).json({error: "Invalid Credentials"});
         // }
 
+        if (!user) {
+            return res.status(400).json({ error: "Invalid Credentials" });
+        }
+
         generateTokenAndSetCookie(user._id, res);
 
         res.status(200).json({
